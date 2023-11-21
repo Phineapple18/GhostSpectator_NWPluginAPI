fcbb15817b8b78d9e707e8566855b129fde0d220
Phineapple18 <78623655+Phineapple18@users.noreply.github.com>
2023-11-21 23:34:00 +0100

    Create CheckSpawnablePatch.cs

---
+++ GhostSpectator/GhostSpectator/Patches/CheckSpawnablePatch.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using HarmonyLib;
+using Respawning;
+using PluginAPI.Core;
+
+namespace GhostSpectator.Patches
+{
+    [HarmonyPatch(typeof(RespawnManager), nameof(RespawnManager.CheckSpawnable))]
+    internal class CheckSpawnablePatch
+    {
+        internal static void Postfix(RespawnManager __instance, ReferenceHub ply, ref bool __result)
+        {
+            if (Player.Get(ply).IsGhost())
+            {
+                __result = true;
+            }
+        }
+    }
+}
