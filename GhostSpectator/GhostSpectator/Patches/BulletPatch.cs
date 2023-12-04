446542aaeaf5fc6d47560bbee6f688392493fc3a
Phineapple18 <78623655+Phineapple18@users.noreply.github.com>
2023-12-04 12:15:12 +0100

    Add files via upload

---
+++ GhostSpectator/GhostSpectator/Patches/BulletPatch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using HarmonyLib;
+using InventorySystem.Items.Firearms.Modules;
+using PluginAPI.Core;
+using UnityEngine;
+
+namespace GhostSpectator.Patches
+{
+    [HarmonyPatch(typeof(StandardHitregBase), nameof(StandardHitregBase.PlaceBulletholeDecal))]
+    internal class BulletPatch
+    {
+        private static bool Prefix(StandardHitregBase __instance, Ray ray, RaycastHit hit)
+        {
+            return !Player.Get(__instance.Hub).IsGhost();
+        }
+    }
+}
