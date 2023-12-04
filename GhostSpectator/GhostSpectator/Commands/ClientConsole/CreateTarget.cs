f661205e4aa55724733c32d57906d3da9a420663
Phineapple18 <78623655+Phineapple18@users.noreply.github.com>
2023-12-04 12:17:18 +0100

    Create CreateTarget.cs

---
+++ GhostSpectator/GhostSpectator/Commands/ClientConsole/CreateTarget.cs
@@ -0,0 +1,119 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AdminToys;
+using CommandSystem;
+using Mirror;
+using NWAPIPermissionSystem;
+using PlayerRoles.FirstPersonControl;
+using PluginAPI.Core;
+
+namespace GhostSpectator.Commands.ClientConsole
+{
+    public class CreateTarget : ICommand, IUsageProvider
+    {
+        public CreateTarget (string command, string description, string[] aliases)
+        {
+            Command = !string.IsNullOrWhiteSpace(command) ? command : _command;
+            Description = !string.IsNullOrWhiteSpace(description) ? description : _description;
+            Aliases = !aliases.IsEmpty() ? aliases : _aliases;
+        }
+
+        public string Command { get; }
+
+        public string[] Aliases { get; }
+
+        public string Description { get; }
+
+        public string[] Usage { get; } = new string[]
+        {
+            "\"dboy\"/\"sport\"/\"bin\""
+        };
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            CommandTranslation translation = CommandTranslation.loadedTranslation;
+            if (Plugin.Singleton == null)
+            {
+                response = translation.NotEnabled;
+                return false;
+            }
+            if (sender == null)
+            {
+                response = translation.SenderNull;
+                return false;
+            }
+            if (!sender.CheckPermission("gs.target"))
+            {
+                response = translation.NoPerms;
+                return false;
+            }
+            Player commandsender = Player.Get(sender);
+            if (!commandsender.IsGhost())
+            {
+                response = translation.NotGhost;
+                return false;
+            }
+            GhostComponent component = commandsender.GetComponent<GhostComponent>();
+            if (component.shootingTargets.Count >= Plugin.Singleton.PluginConfig.TargetLimit)
+            {
+                response = translation.TargetLimit;
+                return false;
+            }
+            IFpcRole fpcRole = commandsender.RoleBase as IFpcRole;
+            if (!fpcRole.FpcModule.IsGrounded)
+            {
+                response = translation.NotGrounded;
+                return false;
+            }
+            foreach (var area in Plugin.shootingAreas)
+            {
+                Log.Debug($"conatins ply = {area.Contains(commandsender.Position)}");
+            }
+
+            if (!Plugin.shootingAreas.Any(a => a.Contains(commandsender.Position)))
+            {
+                response = translation.WrongArea;
+                return false;
+            }
+            if (arguments.IsEmpty())
+            {
+                response = $"{translation.Usage}: {this.DisplayCommandUsage()}.";
+                return false;
+            }
+            AdminToyBase targetBase = null;
+            AdminToyBase target = null;
+            try
+            {
+                NetworkClient.prefabs.Values.First(a => a.TryGetComponent<AdminToyBase>(out targetBase) && targetBase.CommandName == this.targetNames[arguments.At(0)]);
+                target = UnityEngine.Object.Instantiate<AdminToyBase>(targetBase);
+                target.OnSpawned(commandsender.ReferenceHub, arguments);
+                component.shootingTargets.Add(target.netId, target);
+                Log.Debug($"Ghost {commandsender.Nickname} created a shooting target with Id {target.netId}.", Plugin.Singleton.PluginConfig.Debug, $"{Plugin.Singleton.pluginHandler.PluginName}.CreateTarget");
+                response = translation.CreatetargetSuccess.Replace("%id%", target.netId.ToString());
+                return true;
+            }
+            catch (Exception)
+            {
+                response = translation.CreatetargetFail;
+                return false;
+            }
+        }
+
+        internal static readonly string _command = "createtarget";
+
+        internal static readonly string _description = "Create a shooting target.";
+
+        internal static readonly string[] _aliases = new string[] { "ct" };
+
+        private readonly Dictionary<string, string> targetNames = new Dictionary<string, string>()
+        {
+            { "dboy", "TargetDBoy" },
+            { "sport", "TargetSport" },
+            { "bin", "TargetBinary" }
+        };
+    }
+}
