5f731b88ac6c496423f1d634e0f6f3bd8fa21de3
Phineapple18 <78623655+Phineapple18@users.noreply.github.com>
2023-12-04 12:21:33 +0100

    Add files via upload

---
+++ GhostSpectator/GhostSpectator/Commands/RemoteAdminConsole/GhostSpectatorParent.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+using CommandSystem;
+using NorthwoodLib.Pools;
+
+namespace GhostSpectator.Commands.RemoteAdminConsole
+{
+	[CommandHandler(typeof(RemoteAdminCommandHandler))]
+    public class GhostSpectatorParent : ParentCommand
+	{
+		public GhostSpectatorParent()
+		{
+            translation = CommandTranslation.PrepareTranslations();
+            Command = !string.IsNullOrWhiteSpace(translation.ParentCommand) ? translation.ParentCommand : CommandTranslation._parentCommand;
+            Description = !string.IsNullOrWhiteSpace(translation.ParentDescription) ? translation.ParentDescription: CommandTranslation._parentDescription;
+            Aliases = !translation.ParentAliases.IsEmpty() ? translation.ParentAliases : CommandTranslation._parentAliases;
+            this.LoadGeneratedCommands();
+		}
+
+		public override string Command { get; }
+
+		public override string[] Aliases { get; }
+
+        public override string Description { get; }
+
+		public sealed override void LoadGeneratedCommands()
+		{
+            this.RegisterCommand(new Despawn(translation.DespawnCommand, translation.DespawnDescription, translation.DespawnAliases));
+            this.RegisterCommand(new List(translation.ListCommand, translation.ListDescription, translation.ListAliases));
+            this.RegisterCommand(new Spawn(translation.SpawnCommand, translation.SpawnDescription, translation.SpawnAliases));
+        }
+
+		protected override bool ExecuteParent(ArraySegment<string> arguments, ICommandSender sender, out string response)
+		{
+			if (Plugin.Singleton == null)
+			{
+				response = translation.NotEnabled;
+				return false;
+			}
+			StringBuilder stringBuilder = StringBuilderPool.Shared.Rent();
+            stringBuilder.AppendLine($"{Description} {translation.Subcommands}:");
+            foreach (ICommand command in this.AllCommands)
+			{
+				stringBuilder.AppendLine($"- {command.Command} | {translation.Aliases}: {(command.Aliases.Length == 0 ? "" : string.Join(", ", command.Aliases))} | {translation.Description}: {command.Description}");
+            }
+            response = StringBuilderPool.Shared.ToStringReturn(stringBuilder).TrimEnd(Array.Empty<char>());
+			return true;
+		}
+
+        private readonly CommandTranslation translation;
+    }
+}
---
+++ GhostSpectator/GhostSpectator/Commands/RemoteAdminConsole/List.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CommandSystem;
+
+namespace GhostSpectator.Commands.RemoteAdminConsole
+{
+	public class List : ICommand
+	{
+        public List (string command, string description, string[] aliases)
+        {
+            Command = !string.IsNullOrWhiteSpace(command) ? command : _command;
+            Description = !string.IsNullOrWhiteSpace(description) ? description : _description;
+            Aliases = !aliases.IsEmpty() ? aliases : _aliases;
+        }
+
+        public string Command { get; } 
+
+		public string[] Aliases { get; } 
+
+		public string Description { get; } 
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+		{
+			if (Plugin.Singleton == null)
+			{
+				response = CommandTranslation.loadedTranslation.NotEnabled;
+				return false;
+			}
+            response = $"{CommandTranslation.loadedTranslation.GhostList}:" + string.Join("\n- ", from player in GhostSpectator.List select player.Nickname);
+			return true;
+		}
+
+        internal static readonly string _command = "list";
+
+        internal static readonly string _description = "Print list of all Ghosts.";
+
+        internal static readonly string[] _aliases = new string[] { "l" };
+    }
+}
