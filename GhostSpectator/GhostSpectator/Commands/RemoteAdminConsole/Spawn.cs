da0fff9611e5d1363a45b049c8ccb677799faaf5
Phineapple18 <78623655+Phineapple18@users.noreply.github.com>
2024-01-07 17:30:50 +0100

    Update Spawn.cs

--- GhostSpectator/GhostSpectator/Commands/RemoteAdminConsole/Spawn.cs
+++ GhostSpectator/GhostSpectator/Commands/RemoteAdminConsole/Spawn.cs
@@ -78,7 +78,6 @@ namespace GhostSpectator.Commands.RemoteAdminConsole
                 response = translation.DedicatedServer;
                 return false;
             }
-            //validHubs.Remove(validHubs.FirstOrDefault(h => h.isLocalPlayer));
             StringBuilder success = StringBuilderPool.Shared.Rent(); 
             StringBuilder failure = StringBuilderPool.Shared.Rent();
             success.AppendLine($"{translation.SpawnSuccess}:");
