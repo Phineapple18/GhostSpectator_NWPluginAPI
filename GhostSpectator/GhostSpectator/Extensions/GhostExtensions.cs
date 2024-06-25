b8deb92ae4cebdefd422e90175652b4f5b7c77da
Phineapple18 <78623655+Phineapple18@users.noreply.github.com>
2024-06-25 22:35:20 +0200

    Add files via upload

---
+++ GhostSpectator/GhostSpectator/Extensions/GhostExtensions.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PlayerRoles;
+using PluginAPI.Core;
+
+namespace GhostSpectator.Extensions
+{
+    public static class GhostExtensions
+    {
+        public static void Spawn(Player player)
+        {
+            player.TemporaryData.Add(dataName, "spawning");
+            try
+            {
+                player.GetGhostComponent().enabled = true;
+            }
+            catch (Exception)
+            {
+                player.GameObject.AddComponent<GhostComponent>();
+            }
+            Log.Debug($"Player {player.Nickname} has been turned into Ghost.", Config.Debug, PluginName);
+        }
+
+        public static void Despawn(Player player, bool toSpectator = true)
+        {
+            player.GetGhostComponent().enabled = false;
+            if (toSpectator)
+            {
+                player.SetRole(RoleTypeId.Spectator);
+            }
+            player.TemporaryData.Remove(dataName);
+            Log.Debug($"Player {player.Nickname} is no longer a Ghost{(toSpectator ? " and changed role to Spectator" : "")}.", Config.Debug, PluginName);
+        }
+
+        public static bool IsGhost(this ReferenceHub hub)
+        {
+            return Player.Get(hub).IsGhost();
+        }
+
+        public static bool IsGhost(this Player player)
+        {
+            return player != null && player.TemporaryData.StoredData.Any(kvp => kvp.Key == dataName && (string)kvp.Value == "spawned");
+        }
+
+        public static bool IsGhostSpawning(this Player player)
+        {
+            return player != null && player.TemporaryData.StoredData.Any(kvp => kvp.Key == dataName && (string)kvp.Value == "spawning");
+        }
+
+        public static bool IsGhostDespawning(this Player player)
+        {
+            return player != null && player.TemporaryData.StoredData.Any(kvp => kvp.Key == dataName && (string)kvp.Value == "despawning");
+        }
+
+        public static GhostComponent GetGhostComponent(this Player player)
+        {
+            return player.GameObject.GetComponent<GhostComponent>();
+        }
+
+        public const string dataName = "IsGhostSpectator";
+
+        public static IEnumerable<Player> GhostPlayerList => Player.GetPlayers().Where(p => p.IsGhost());
+        private static Config Config => Plugin.Singleton.pluginConfig;
+        private static string PluginName => Plugin.Singleton.pluginHandler.PluginName;
+    }
+}
---
+++ GhostSpectator/GhostSpectator/Extensions/OtherExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AdminToys;
+using InventorySystem.Items;
+using Mirror;
+using PluginAPI.Core;
+using UnityEngine;
+
+namespace GhostSpectator.Extensions
+{
+    internal static class OtherExtensions
+    {
+        internal static void DestroyShootingTarget(GhostComponent component, AdminToyBase target)
+        {
+            NetworkServer.Destroy(target.gameObject);
+            component.ShootingTargets.Remove(target);
+            Log.Debug($"Destroyed shooting target {target.name} with ID {target.netId}.", Config.Debug, PluginName);
+        }
+
+        public static bool IsGhostItem(this ItemBase item)
+        {
+            return item != null && GhostItemList.Contains(item);
+        }
+
+        internal static readonly Dictionary<string, KeyValuePair<string, string>> voiceChats = new()
+        {
+            { "scp", new("ListenScpChat", "SCPs") },
+            { "dead", new("ListenSpectator", "Spectators") },
+            { "ghost", new("ListenGhosts", "Ghosts") }
+        };
+
+        internal static HashSet<ItemBase> GhostItemList { get; } = new();
+        internal static List<Vector3> SpawnPositions { get; private set; } = new();
+        internal static List<Bounds> ShootingRanges { get; private set; } = new();
+        private static Config Config => Plugin.Singleton.pluginConfig;
+        private static string PluginName => Plugin.Singleton.pluginHandler.PluginName;
+    }
+}
